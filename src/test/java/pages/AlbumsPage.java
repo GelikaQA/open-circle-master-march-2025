721ef02511857252b899f5be6dab8eb7acc30abf
AndroidBeard <arkadiy.popovtest@gmail.com>
2025-06-17 22:28:29 +0200

    Add 2 new Albums locators

--- src/test/java/pages/AlbumsPage.java
+++ src/test/java/pages/AlbumsPage.java
@@ -8,4 +8,6 @@ public class AlbumsPage {
     private static final String CLOSE_BUTTON_NEW_ALBUM_WINDOW = "xpath=//span[@role='img']";
     private static final String CANCEL_BUTTON_NEW_ALBUM_WINDOW = "xpath=//span[text()='Cancel']";
     private static final String CREATE_BUTTON_NEW_ALBUM_WINDOW = "xpath=//span[text()='Create']";
+    private static final String POP_UP_MSG_IF_ALBUM_ALREADY_EXISTS = "xpath=//span[text()='Album with this name exists']";
+    private static final String POP_UP_MSG_WHEN_ALBUM_IS_CREATED = "xpath=//div[@class='ant-notification-notice-with-icon']";
 }

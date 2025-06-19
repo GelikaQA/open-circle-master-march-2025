8e0e7d56086af867203fb0cfc31ebf18bd172ea5
ValeriiaSQA <teresenkovaleria94@gmail.com>
2025-06-19 12:14:18 -0700

    Automated TC-07 empty First Name field and added new XPath in CreateCircleSteps file

--- src/test/java/pages/CreateCirclePage.java
+++ src/test/java/pages/CreateCirclePage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebElement;
 import static org.junit.Assert.assertTrue;
 import static tools.CommonTools.getByObject;
 
-public class CreateCirclePage extends BasePage{
+public class CreateCirclePage extends BasePage {
     private static final String ADMIN_PASSWORD_INPUT_FIELD = "xpath=//input[contains(@class, 'form_form_control')]";
     private static final String ADMIN_PAGE_SUBMIT_BUTTON = "xpath=//button[@type='submit']";
     private static final String CREATE_CIRCLE_TITLE_HEADER = "xpath=//h1[contains(@class, 'login_auth_title')]";
@@ -18,15 +18,16 @@ public class CreateCirclePage extends BasePage{
     private static final String CREATE_CIRCLE_PASSWORD_INPUT_FIELD = "id=password";
     private static final String CREATE_CIRCLE_CREATE_BUTTON = "xpath=//button[contains(@class, 'login_submit')]";
     private static final String CREATE_CIRCLE_SIGN_IN_HYPERLINK = "xpath=//button[contains(@class, 'login_link')][1]";
-    private static final String CREATE_CIRCLE_JOIN_CIRCLE_HYPERLINK =  "xpath=//button[contains(@class, 'login_link')][2]";
+    private static final String CREATE_CIRCLE_JOIN_CIRCLE_HYPERLINK = "xpath=//button[contains(@class, 'login_link')][2]";
     private static final String CREATE_CIRCLE_COPY_ERROR_POPUP = "xpath=//div[contains(@class, 'ant-notification ant-notification-topRight')]//span[text()='Generate PassCode firstly']";
     private static final String CREATE_CIRCLE_NON_UNIQUE_EMAIL_ERROR_POPUP = "xpath=//div[contains(@class, 'ant-notification ant-notification-topRight')]//span[text()='A user with such an email exists.']";
     private static final String CREATE_CIRCLE_NON_UNIQUE_CIRCLE_NAME_ERROR_POPUP = "xpath=//div[@class='ant-notification-notice-with-icon']//span[text()='A Circle with this name exists.']";
     private static final String CREATE_CIRCLE_FIELD_CONSTRAINT_ERROR_MESSAGE = "xpath=//ul[@class='form_text_danger__LQWyf']/li[text()='Field accepts alphabetical char and digits, no special chars allowed, limited to 32 characters']";
-//  private static final String LOGIN_CREATE_CIRCLE_SUCCESS_POPUP = "xpath=//div[@class='ant-notification-notice-content']//span[text='You have successfully registered.']""; This XPath is for the success message that pops up in Login page after Circle is successfully created
+    private static final String CREATE_CIRCLE_ERROR_MESSAGE_FOR_EMPTY_FIRST_NAME_FIELD = "xpath=//div[@class='form_form_group__gzDYz'][label[@for='firstName']]//li[text()='This input is required.']";
+    //  private static final String LOGIN_CREATE_CIRCLE_SUCCESS_POPUP = "xpath=//div[@class='ant-notification-notice-content']//span[text='You have successfully registered.']""; This XPath is for the success message that pops up in Login page after Circle is successfully created
 
     public static String getAdminPasswordInputField() {
-         return ADMIN_PASSWORD_INPUT_FIELD;
+        return ADMIN_PASSWORD_INPUT_FIELD;
     }
 
     public static String getAdminPageSubmitButton() {
@@ -65,6 +66,9 @@ public class CreateCirclePage extends BasePage{
         return CREATE_CIRCLE_FIELD_CONSTRAINT_ERROR_MESSAGE;
     }
 
+    public static String getCreateCircleErrorMessageForEmptyFirstNameField() {
+        return CREATE_CIRCLE_ERROR_MESSAGE_FOR_EMPTY_FIRST_NAME_FIELD;
+    }
 
 
     public void openCreateCirclePage() {
@@ -160,5 +164,18 @@ public class CreateCirclePage extends BasePage{
         WebElement foundElement = driver.findElement(getByObject(getCreateCircleFieldConstraintErrorMessage()));
         assertTrue(foundElement.isDisplayed());
     }
-}
 
+
+
+    public void assertErrorMessageForEmptyFirstNameFieldOnCreateCirclePage(String error) {
+        wait.forElementToBeDisplayed(
+                10,
+                getByObject(getCreateCircleErrorMessageForEmptyFirstNameField()),
+                "Error message");
+        WebElement foundElement = driver.findElement(getByObject(getCreateCircleErrorMessageForEmptyFirstNameField()));
+        String elementText = foundElement.getText();
+
+        String message = "Text '" + error + "' 'in " + getCreateCircleErrorMessageForEmptyFirstNameField() + " is presented.";
+        assertTrue(message, elementText.contains(error));
+    }
+}
--- src/test/java/steps/CreateCircleSteps.java
+++ src/test/java/steps/CreateCircleSteps.java
@@ -64,4 +64,10 @@ public class CreateCircleSteps {
     public void theyArePromptedByAnErrorMessage() {
         createCirclePage.promptedByErrorMessageOnCreateCirclePage();
     }
+
+
+    @Then("error message pops-up {string}")
+    public void errorMessagePopsUp(String error) {
+        createCirclePage.assertErrorMessageForEmptyFirstNameFieldOnCreateCirclePage(error);
+    }
 }

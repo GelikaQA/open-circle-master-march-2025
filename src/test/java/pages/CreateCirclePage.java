631b335e92aade0bea40214e87e59375bf0f75b6
iamsudha <sudhaaraveeti@gmail.com>
2025-07-11 15:24:11 -0700

    Refactored and added TC7

--- src/test/java/pages/CreateCirclePage.java
+++ src/test/java/pages/CreateCirclePage.java
@@ -9,23 +9,14 @@ import static tools.CommonTools.getByObject;
 public class CreateCirclePage extends BasePage {
     private static final String ADMIN_PASSWORD_INPUT_FIELD = "xpath=//input[contains(@class, 'form_form_control')]";
     private static final String ADMIN_PAGE_SUBMIT_BUTTON = "xpath=//button[@type='submit']";
-    private static final String CREATE_CIRCLE_TITLE_HEADER = "xpath=//h1[contains(@class, 'login_auth_title')]";
     private static final String CIRCLE_NAME_FIELD_ON_CREATE_CIRCLE_PAGE = "id=circleId";
-    private static final String CREATE_CIRCLE_PASSCODE_INPUT_FIELD = "id=passCode";
     private static final String GENERATE_PASSCODE_BUTTON_ON_CREATE_CIRCLE_PAGE = "xpath=//button[@class='ant-btn css-o2xhy3 ant-btn-primary'][1]";
-    private static final String COPY_PASSCODE_BUTTON_ON_CREATE_CIRCLE_PAGE = "xpath=//button[@class='ant-btn css-o2xhy3 ant-btn-primary'][2]";
     private static final String FIRSTNAME_ON_CREATE_CIRCLE_PAGE = "id=firstName";
     private static final String LASTNAME_ON_CREATE_CIRCLE_PAGE = "id=lastName";
     private static final String EMAIL_ON_CREATE_CIRCLE_PAGE = "id=email";
     private static final String CREATE_CIRCLE_PASSWORD_INPUT_FIELD = "id=password";
     private static final String CREATE_CIRCLE_CREATE_BUTTON = "xpath=//button[contains(@class, 'login_submit')]";
-    private static final String CREATE_CIRCLE_SIGN_IN_HYPERLINK = "xpath=//button[contains(@class, 'login_link')][1]";
-    private static final String CREATE_CIRCLE_JOIN_CIRCLE_HYPERLINK = "xpath=//button[contains(@class, 'login_link')][2]";
-    private static final String CREATE_CIRCLE_COPY_ERROR_POPUP = "xpath=//div[contains(@class, 'ant-notification ant-notification-topRight')]//span[text()='Generate PassCode firstly']";
-    private static final String CREATE_CIRCLE_NON_UNIQUE_EMAIL_ERROR_POPUP = "xpath=//div[contains(@class, 'ant-notification ant-notification-topRight')]//span[text()='A user with such an email exists.']";
-    private static final String CREATE_CIRCLE_NON_UNIQUE_CIRCLE_NAME_ERROR_POPUP = "xpath=//div[@class='ant-notification-notice-with-icon']//span[text()='A Circle with this name exists.']";
-    private static final String CREATE_CIRCLE_FIELD_CONSTRAINT_ERROR_MESSAGE = "xpath=//ul[@class='form_text_danger__LQWyf']/li[text()='Field accepts alphabetical char and digits, no special chars allowed, limited to 32 characters']";
-    private static final String CREATE_CIRCLE_ERROR_MESSAGE_FOR_EMPTY_FIRST_NAME_FIELD = "xpath=//div[label[@for='firstName']]//li";
+    private static final String CREATE_CIRCLE_FIRST_NAME_VALIDATION = "xpath=//div[label[@for='firstName']]//li";
     //  private static final String LOGIN_CREATE_CIRCLE_SUCCESS_POPUP = "xpath=//div[@class='ant-notification-notice-content']//span[text='You have successfully registered.']""; This XPath is for the success message that pops up in Login page after Circle is successfully created
 
     public static String getAdminPasswordInputField() {
@@ -64,12 +55,8 @@ public class CreateCirclePage extends BasePage {
         return CREATE_CIRCLE_CREATE_BUTTON;
     }
 
-    public static String getCreateCircleFieldConstraintErrorMessage() {
-        return CREATE_CIRCLE_FIELD_CONSTRAINT_ERROR_MESSAGE;
-    }
-
-    public static String getCreateCircleErrorMessageForEmptyFirstNameField() {
-        return CREATE_CIRCLE_ERROR_MESSAGE_FOR_EMPTY_FIRST_NAME_FIELD;
+    public static String getCreateCircleFirstNameValidation() {
+        return CREATE_CIRCLE_FIRST_NAME_VALIDATION;
     }
 
     public void openCreateCirclePage() {
@@ -157,27 +144,27 @@ public class CreateCirclePage extends BasePage {
         foundElement.click();
     }
 
-    public void anErrorMessagePopulatesBelowTheFirstNameFieldOnTheCreateCirclePage(String error) {
+//    public void anErrorMessagePopulatesBelowTheFirstNameFieldOnTheCreateCirclePage(String error) {
+//        wait.forElementToBeDisplayed(
+//                10,
+//                getByObject(getCreateCircleFieldConstraintErrorMessage()),
+//                "Error message");
+//        WebElement foundElement = driver.findElement(getByObject(getCreateCircleFieldConstraintErrorMessage()));
+//        String elementText = foundElement.getText();
+//
+//        String message = "Text '" + error + "' 'in " + getCreateCircleFieldConstraintErrorMessage() + " is presented.";
+//        assertTrue(message, elementText.contains(error));
+//    }
+
+    public void checkMessageExistsForFirstNameFieldOnCreateCirclePage(String error) {
         wait.forElementToBeDisplayed(
                 10,
-                getByObject(getCreateCircleFieldConstraintErrorMessage()),
+                getByObject(getCreateCircleFirstNameValidation()),
                 "Error message");
-        WebElement foundElement = driver.findElement(getByObject(getCreateCircleFieldConstraintErrorMessage()));
+        WebElement foundElement = driver.findElement(getByObject(getCreateCircleFirstNameValidation()));
         String elementText = foundElement.getText();
 
-        String message = "Text '" + error + "' 'in " + getCreateCircleFieldConstraintErrorMessage() + " is presented.";
-        assertTrue(message, elementText.contains(error));
-    }
-
-    public void assertErrorMessageForEmptyFirstNameFieldOnCreateCirclePage(String error) {
-        wait.forElementToBeDisplayed(
-                10,
-                getByObject(getCreateCircleErrorMessageForEmptyFirstNameField()),
-                "Error message");
-        WebElement foundElement = driver.findElement(getByObject(getCreateCircleErrorMessageForEmptyFirstNameField()));
-        String elementText = foundElement.getText();
-
-        String message = "Text '" + error + "' 'in " + getCreateCircleErrorMessageForEmptyFirstNameField() + " is presented.";
+        String message = "Text '" + error + "' 'in " + getCreateCircleFirstNameValidation() + " is presented.";
         assertTrue(message, elementText.contains(error));
     }
 }

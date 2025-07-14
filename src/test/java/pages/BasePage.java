83a52205ddee88bc347e2aac0a103d3385efb2f4
Wirelessbow <139515300+Wirelessbow@users.noreply.github.com>
2025-07-14 11:25:28 -0700

    JoinCirclePage. Test case 7 and 23(copy password) is automated. Addit… (#57)
    * JoinCirclePage. Test case 7 and 23(copy password) is automated. Additional test case 24 with context menu is waiting to fix it
    
    * JoinCirclePage. Small fixes on method's names. Instead of one generic Xpath added 6 for each field. TC 7, 23, 24(partially) and 25 are automated.
    
    * JoinCirclePage. Small fixes on method's names. Instead of one generic Xpath added 6 for each field. TC 7, 23, 24(partially) and 25 are automated.
    
    * JoinCirclePage. Small fixes on method's names. Instead of one generic Xpath added 6 for each field. TC 7, 23, 24(partially) and 25 are automated.
    
    * JoinCirclePage. Small fixes on method's names. Instead of one generic Xpath added 6 for each field. TC 7, 23, 24(partially) and 25 are automated.
    
    * JoinCirclePage. Small fixes on method's names. Instead of one generic Xpath added 6 for each field. TC 7 connected with TC 6. TC 23, 24(partially) and 25 are automated.
    
    * JoinCirclePage. Small fixes on method's names. Instead of one generic Xpath added 6 for each field. TC 7 connected with TC 6. TC 23, 24(partially) and 25 are automated.
    
    * JoinCirclePage. Small fixes on method's names. Instead of one generic Xpath added 6 for each field. TC 7 connected with TC 6. TC 23, 24(partially) and 25 are automated.

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -18,10 +18,12 @@ public class BasePage {
     HashMap<String, Object> context;
 
     public static final String POP_UP_WINDOW_MESSAGE_LOCATOR = "xpath=//div[@class='ant-notification-notice-message']";
+    public static final String PASSWORD_INPUT_FIELD = "name=password";
 
     public static String getPopUpWindowMessageLocator(){
         return POP_UP_WINDOW_MESSAGE_LOCATOR;
     }
+    public static String getPasswordInputField() { return PASSWORD_INPUT_FIELD; }
 
     public BasePage() {
         this.driver = Setup.driver;
@@ -152,5 +154,12 @@ public class BasePage {
     public static String generateUniqueName(String name) {
         String timestamp = String.valueOf(System.currentTimeMillis());
         return name + timestamp;
+    }
+    public void PasswordIsMasked() {
+        wait.forElementToBeDisplayed(10, getByObject(getPasswordInputField()),
+                "Password is masked");
+        WebElement passwordField = driver.findElement(getByObject(getPasswordInputField()));
+        String typeAttribute = passwordField.getAttribute("type");
+        assertTrue("The password field is not masked. Can't find attribute in tag 'type=password' ", typeAttribute.equals("password"));
     }
 }
--- src/test/java/pages/JoinCirclePage.java
+++ src/test/java/pages/JoinCirclePage.java
@@ -6,6 +6,15 @@ import tools.PropertiesLoader;
 import static org.junit.Assert.assertTrue;
 import static tools.CommonTools.getByObject;
 
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.Keys;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+
+import static org.junit.Assert.assertNotEquals;
+
 public class JoinCirclePage extends BasePage {
     private static final String JOIN_CIRCLE_ENTIRE_PAGE_VERIFICATION = "xpath=//div[contains(@class,'login_auth_body')]//h1[text()='Join Circle']";
     private static final String JOIN_CIRCLE_CIRCLE_NAME_INPUT_FIELD = "name=circleId";
@@ -18,15 +27,17 @@ public class JoinCirclePage extends BasePage {
     private static final String JOIN_CIRCLE_SIGN_IN_REDIRECT_BUTTON = "xpath=//button[@type='button']//span[text()='Sign in']";
     private static final String JOIN_CIRCLE_CREATE_CIRCLE_REDIRECT_BUTTON = "xpath=//button[@type='button']//span[text()='Create Circle']";
     private static final String JOIN_CIRCLE_POP_UP_MESSAGE_SUCCESS_REGISTRATION = "xpath=//div[@class='ant-notification-notice-description']//span[text()='You have successfully registered.']";
-    private static final String JOIN_CIRCLE_POP_UP_MESSAGE_INCORRECT_PASSCODE = "xpath=//div[@class='ant-notification-notice-description']//span[text()='Incorrect Passcode. Please try again']";
-    private static final String JOIN_CIRCLE_FN_FIELD_MESSAGE_FIELD_ACCEPTS = "xpath=//ul[contains(@class, 'form_text_danger')]//li[contains(text(), 'Field accepts alphabetical')]";
-    private static final String JOIN_CIRCLE_FN__CIRCLE_EMAIL_PASSWORD_FIELD_MESSAGE_INPUT_IS_REQUIRED = "xpath=//ul[contains(@class,'form_text_danger')]//li[text()='This input is required.']";
-    private static final String JOIN_CIRCLE_LN_FIELD_MESSAGE_FIELD_ACCEPTS = "xpath=//ul[contains(@class,'form_text_danger')]//li[starts-with(text(),'Field accepts alphabetical')]";
-    private static final String JOIN_CIRCLE_LN_FIELD_MESSAGE_INPUT_IS_REQUIRED = "xpath=//ul[contains(@class,'form_text_danger')]//li[contains(text(),'This input is required.')]";
+    private static final String JOIN_CIRCLE_POP_UP_MESSAGE_INCORRECT_PASSCODE = "xpath=//div[@class='ant-notification-notice-description']/span";
+    private static final String JOIN_CIRCLE_FIRST_NAME_FIELD_MESSAGE_FIELD_ACCEPTS = "xpath=//ul[contains(@class, 'form_text_danger')]//li[contains(text(), 'Field accepts alphabetical')]";
+    private static final String JOIN_CIRCLE_CIRCLE_FIELD_MESSAGE = "xpath=//label[@for='circleId']/following-sibling::ul[contains(@class,'form_text_danger')]/li";
+    private static final String JOIN_CIRCLE_PASSCODE_FIELD_MESSAGE = "xpath=//label[@for='passCode']/following-sibling::ul[contains(@class,'form_text_danger')]/li";
+    private static final String JOIN_CIRCLE_FIRST_NAME_FIELD_MESSAGE = "xpath=//label[@for='firstName']/following-sibling::ul[contains(@class,'form_text_danger')]/li";
+    private static final String JOIN_CIRCLE_LAST_NAME_FIELD_MESSAGE = "xpath=//label[@for='lastName']/following-sibling::ul[contains(@class,'form_text_danger')]/li";
+    private static final String JOIN_CIRCLE_EMAIL_FIELD_MESSAGE = "xpath=//label[@for='email']/following-sibling::ul[contains(@class,'form_text_danger')]/li";
+    private static final String JOIN_CIRCLE_PASSWORD_FIELD_MESSAGE = "xpath=//label[@for='password']/following-sibling::ul[contains(@class,'form_text_danger')]/li";
     private static final String JOIN_CIRCLE_EMAIL_FIELD_MESSAGE_INVALID_EMAIL = "xpath=//ul[contains(@class,'form_text_danger')]//li[contains(text(),'Invalid email address')]";
     private static final String JOIN_CIRCLE_PASSWORD_FIELD_MESSAGE_FIELD_SHOULD = "xpath=//ul[contains(@class,'form_text_danger')]//li[contains(text(),'Field should contain at least one')]";
     private static final String JOIN_CIRCLE_POP_UP_MESSAGE_EMAIL_EXIST = "xpath=//span[text()='A user with such an email exists.']";
-    private static final String JOIN_CIRCLE_PASSWORD_FIELD_ENTERED_VALUE_123456zZ = "xpath=//input[@id='password' and @value='123456zZ']";
     private static final String JOIN_CIRCLE_POP_UP_MESSAGE_INCORRECT_CIRCLE_NAME = "xpath=//div[contains(@class,'ant-notification-notice-description')]//span[text()='Incorrect Circle Name. Please try again']";
 
     public static String getJoinCircleCircleNameInputField() {
@@ -65,8 +76,8 @@ public class JoinCirclePage extends BasePage {
         return JOIN_CIRCLE_POP_UP_MESSAGE_INCORRECT_PASSCODE;
     }
 
-    public static String getJoinCirclePopUpMessageIncorrectFirstName() {
-        return JOIN_CIRCLE_LN_FIELD_MESSAGE_FIELD_ACCEPTS;
+    public static String getJoinCircleMessageIncorrectFirstName() {
+        return JOIN_CIRCLE_FIRST_NAME_FIELD_MESSAGE;
     }
 
     public void openJoinCirclePage() {
@@ -80,7 +91,7 @@ public class JoinCirclePage extends BasePage {
         foundElement.sendKeys(circleName);
     }
 
-    public void enterPasscodeFieldOnTheJoinCirclePage(String Passcode) {
+    public void enterPasscodeOnJoinCirclePage(String Passcode) {
         wait.forElementToBeDisplayed(10, getByObject(getJoinCirclePasscodeInputField()),
                 "Passcode input field");
         WebElement foundElement = driver.findElement(getByObject(getJoinCirclePasscodeInputField()));
@@ -110,19 +121,19 @@ public class JoinCirclePage extends BasePage {
 
     public void enterPasswordOnJoinCirclePage(String password) {
         wait.forElementToBeDisplayed(10, getByObject(getJoinCirclePasswordInputField()),
-                "PASSWORD INPUT");
+                "Password input field");
         WebElement passwordField = driver.findElement(getByObject(getJoinCirclePasswordInputField()));
         passwordField.sendKeys(password);
     }
 
-    public void theyPressButtonJoinOnTheJoinCirclePage() {
+    public void PressButtonJoinOnJoinCirclePage() {
         wait.forElementToBeDisplayed(10, getByObject(getJoinCircleJoinSubmitButton()),
                 "SIGN UP BUTTON");
         WebElement signUpButton = driver.findElement(getByObject(getJoinCircleJoinSubmitButton()));
         signUpButton.click();
     }
 
-    public void assertTheyAreStillOnJoinCirclePage() {
+    public void assertTheyAreOnJoinCirclePage() {
         wait.forElementToBeDisplayed(10, getByObject(getJoinCircleEntirePageVerification()),
                 "JOIN CIRCLE PAGE LOCATE/VERIFY");
         WebElement foundElement = driver.findElement(getByObject(getJoinCircleEntirePageVerification()));
@@ -139,13 +150,70 @@ public class JoinCirclePage extends BasePage {
         assertTrue(message, elementText.contains(incorrectPasscode));
     }
 
-    public void assertWarningMessageForFirstNameOnJoinCirclePageIsDisplayed(String incorrectFirstName) {
-        wait.forElementToBeDisplayed(10, getByObject(getJoinCirclePopUpMessageIncorrectFirstName()),
-                "Field accepts alphabetical char and digits, no special chars allowed, limited to 32 character");
-        WebElement foundElement = driver.findElement(getByObject(getJoinCirclePopUpMessageIncorrectFirstName()));
+        public void assertFirstNameFieldWarningMessageIsDisplayed(String incorrectFirstName) {
+        wait.forElementToBeDisplayed(10, getByObject(getJoinCircleMessageIncorrectFirstName()),
+                "Field accepts alphabetical char and digits, no special chars allowed, limited to 32 character or This input is required");
+        WebElement foundElement = driver.findElement(getByObject(getJoinCircleMessageIncorrectFirstName()));
         String elementText = foundElement.getText();
 
-        String message = "Text '" + incorrectFirstName + "' in " + getJoinCirclePopUpMessageIncorrectFirstName() + " is not presented. 'Actual text is '" + elementText + "'";
+        String message = "Text '" + incorrectFirstName + "' in " + getJoinCircleMessageIncorrectFirstName() + " is not presented. 'Actual text is '" + elementText + "'";
         assertTrue(message, elementText.contains(incorrectFirstName));
+    }
+
+    public void CopyHiddenPasswordOnJoinCirclePage() {
+        wait.forElementToBeDisplayed(10, getByObject(getJoinCirclePasswordInputField()),
+                "Password input field");
+        WebElement passwordField = driver.findElement(getByObject(getJoinCirclePasswordInputField()));
+
+        Actions actions = new Actions(driver);
+        actions
+                .moveToElement(passwordField)
+                .click()
+                .keyDown(Keys.CONTROL)
+                .sendKeys("a", "c")
+                .keyUp(Keys.CONTROL)
+                .perform();
+    }
+
+    public void PasswordIsNotCopiedIntoClipboardFromPasswordField() {
+        String clipboardContent = getClipboardContents();
+        String actualPassword = getPasswordInputValue(); // Gets the password field value
+        System.out.println("Clipboard: " + clipboardContent);
+        System.out.println("Entered password: " + actualPassword);
+
+        assertNotEquals("ERROR: password should not be copied into clipboard", actualPassword, clipboardContent); //Asserts that clipboard ≠ password
+    }
+
+    private String getClipboardContents() { // Additional method to get clipboard data
+        try {
+            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+            return (String) clipboard.getData(DataFlavor.stringFlavor);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private String getPasswordInputValue() { // Additional method get text from the password input field
+        WebElement passwordField = driver.findElement(getByObject(getJoinCirclePasswordInputField()));
+        return passwordField.getAttribute("value");
+    }
+
+    public void EnterValidPasswordInPasswordFieldOnJoinCirclePage(String password) {
+        wait.forElementToBeDisplayed(10, getByObject(getJoinCirclePasswordInputField()),
+                "Password input field");
+        driver.findElement(getByObject(getJoinCirclePasswordInputField())).sendKeys(password);
+    }
+
+    //    for next test case
+    public void OpenedContextMenuOnJoinCirclePage() {
+        WebElement passwordField = driver.findElement(getByObject(getJoinCirclePasswordInputField()));
+
+        Actions actions = new Actions(driver);
+        actions
+                .moveToElement(passwordField)
+                .contextClick() // Mouse right click button simulation
+                .perform();
+
     }
 }

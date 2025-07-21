c83a210d65a3c0157a7845bc86e3e3102fba7383
Kamaliya <kamaliya.test@gmail.com>
2025-07-20 19:27:31 -0700

    Added new TCs

--- src/test/java/pages/ChangePasswordPage.java
+++ src/test/java/pages/ChangePasswordPage.java
@@ -16,7 +16,11 @@ public class ChangePasswordPage extends BasePage{
     private static final String CURRENT_PASSWORD_HIDDEN_BY_DOTS = "id=oldPassword";
     private static final String NEW_PASSWORD_HIDDEN_BY_DOTS = "id=newPassword";
     private static final String WARNING_MESSAGE_ON_CHANGE_PASSWORD_PAGE = "xpath=//div[@class='ant-notification-notice-content']";
+    private static final String NEW_INVALID_PASSWORD_ERROR_MESSAGE = "xpath=//div[@id='newPassword_help']";
 
+    public static String getNewInvalidPasswordErrorMessage() {
+        return NEW_INVALID_PASSWORD_ERROR_MESSAGE;
+    }
     public static String getProfileIcon() {
         return PROFILE_ICON;
     }
@@ -90,5 +94,13 @@ public class ChangePasswordPage extends BasePage{
 
         assertTrue(warning, elementText.contains(warning));
         driver.findElement(getByObject(getClosePopupMessageLocator())).click();
+    }
+
+    public void assertErrorMessageUnderNewPasswordField(String InvalidPassword) {
+        wait.forElementToBeDisplayed(10,
+                getByObject(getNewInvalidPasswordErrorMessage()),
+                "Error message");
+        WebElement foundElement = driver.findElement(getByObject(getNewInvalidPasswordErrorMessage()));
+        assertTrue(foundElement.isDisplayed());
     }
 }
